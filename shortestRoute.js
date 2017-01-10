d6f730a12dbdb1b1e04b5f333234ce09709f1b30
mDalessandro <mattd942@gmail.com>
2017-01-10 14:43:41 -0500

    init

---
+++ shortestRoute.js
@@ -0,0 +1,60 @@
+/*
+PROMPT
+
+Jennifer is a fashion buyer for an international apparel and footwear retailer. She
+often finds herself traveling to discover new industry trends. To keep her time
+on the road to a minimum, she is enlisting the help of a programmer friend (you)
+to help her find the shortest route between her current location and her next
+destination.
+
+Your program should accept the following inputs:
+
+1. A list containing the city pairs and the distance between them
+
+For example:
+
+Boston,New York,50
+
+Boston,Hartford,25
+
+Hartford,New York,35
+
+New York,Philadelphia,40
+
+Note that each city pair in the input list should be considered bi-
+directional. So (Boston,New York,50) means that it is valid to travel from
+
+Boston to New York at a distance of 50 AND it is also valid to travel from
+
+New York to Boston at a distance of 50.
+
+2. The name of the starting city
+
+For example: Boston
+
+3. The name of the destination city
+
+For example: Philadelphia
+
+Your program should return a list of the cities that comprise the shortest route:
+
+1. For the example above, your program would return:
+
+Boston,New York,Philadelphia
+
+You can write the program in any language you choose. However, Jennifer
+needs to be able to run your program on her laptop computer. So in addition to
+the source code, you should provide her with documentation on how to compile
+and execute your code. Your documentation should also describe what, if any,
+testing was performed to verify your solution.  
+ 
+*/
+
+
+
+
+
+
+
+
+
